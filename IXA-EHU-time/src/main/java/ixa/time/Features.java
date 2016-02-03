d8beef5365cd8fddf1e718be179703a6da2b1eb0
EgoLaparra <egoitz.laparra@ehu.es>
2016-02-03 16:30:54 +0100

    more commits

---
+++ IXA-EHU-time/src/main/java/ixa/time/Features.java
@@ -0,0 +1,66 @@
+package ixa.time;
+
+import java.util.Iterator;
+import java.util.List;
+
+import ixa.kaflib.*;
+import ixa.kaflib.KAFDocument.Layer;
+
+public class Features {
+
+	public String lemma;
+	public String pos;
+	public String phrase;
+	public String polarity;
+	public String tense;
+	public String aspect;
+	
+	private void Lemma (Term term) {
+		this.lemma = term.getLemma();
+	}
+	
+	private void PoS (Term term) {
+		this.pos = term.getPos();
+	}
+	
+    private String walkConsituency (Term t, TreeNode node, String label) {
+    	if (node.isTerminal()) {
+    		Terminal term = (Terminal) node;
+    		if (term.getSpan().getFirstTarget().getId().equals(t.getId())){
+    			return label;
+    		}
+    	}
+    	else {
+    		NonTerminal nonTerm = (NonTerminal) node;
+    		List<TreeNode> children = node.getChildren();
+    		Iterator childrenIterator = children.iterator();
+    		while (childrenIterator.hasNext()) {
+    			TreeNode child = (TreeNode) childrenIterator.next();
+    			walkConsituency(t, child, nonTerm.getLabel());
+    		}
+    	}
+    	
+    	return null;
+    }
+    
+	private void Phrase (Term term, List<Annotation> constituency) {
+    	Iterator trees = constituency.iterator();
+    	while (trees.hasNext()) {
+        	Tree tree = (Tree) trees.next();
+        	NonTerminal treeNonTerminal = (NonTerminal) tree.getRoot();
+        	this.phrase = walkConsituency(term, tree.getRoot(), treeNonTerminal.getLabel());
+    	}
+	}
+	
+	private void Polarity () {
+		
+	}
+	
+	private void Tense () {
+		
+	}
+	
+	private void Aspect () {
+		
+	}
+}
